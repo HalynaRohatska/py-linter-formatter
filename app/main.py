d1977905d98f7668e6dca505ff6c4f3fa885fe98
Halyna Rohatska <rohatskahr@gmail.com>
2024-06-09 21:40:55 +0300

    implement format_linter_error

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    formated_error = {
+        "source": "flake8"
+    }
+    for key, value in error.items():
+        if key == "line_number":
+            formated_error["line"] = value
+        elif key == "column_number":
+            formated_error["column"] = value
+        elif key == "text":
+            formated_error["message"] = value
+        elif key == "code":
+            formated_error["name"] = value
+    return formated_error
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
